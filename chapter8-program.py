7eb25f116cf97f6a1a03166b1628ee3c33d064a1
Kalel <kalelacez@gmail.com>
2026-07-13 08:16:05 +0100

    Add table parameter to printTable

--- chapter8-program.py
+++ chapter8-program.py
@@ -2,21 +2,21 @@ tableData = [['apples', 'oranges', 'cherries', 'banana'],
              ['Alice', 'Bob', 'Carol', 'David'],
              ['dogs', 'cats', 'moose', 'goose']]
 
-def printTable():
+def printTable(table):
 
-    colWidths = [0] * len(tableData)
-    listLen = len(tableData[0])
+    colWidths = [0] * len(table)
+    listLen = len(table[0])
 
     for i in range(len(colWidths)):
         for x in range(listLen):
-            if len(tableData[i][x]) > colWidths[i]:
-                colWidths[i] = len(tableData[i][x])
+            if len(table[i][x]) > colWidths[i]:
+                colWidths[i] = len(table[i][x])
 
     print(colWidths)
 
     for i in range(listLen):
         print()        
-        for y in range(len(tableData)):
-            print(tableData[y][i].rjust(colWidths[y]), end=" ")
+        for y in range(len(table)):
+            print(table[y][i].rjust(colWidths[y]), end=" ")
 
-printTable()
+printTable(tableData)
