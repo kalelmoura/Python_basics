2c5ed33961d635c2af21f47816b05db41a81e875
Kalel <kalelacez@gmail.com>
2026-07-17 23:01:42 +0100

    py test

--- test.py
+++ test.py
@@ -1,2 +1,21 @@
-def tableprinter():
-    
+import re
+
+
+def is_strong_password(password):
+    uppercase_regex = re.compile(r'[A-Z]')
+    lowercase_regex = re.compile(r'[a-z]')
+    digit_regex = re.compile(r'\d')
+
+    if len(password) < 8:
+        return False
+
+    if uppercase_regex.search(password) is None:
+        return False
+
+    if lowercase_regex.search(password) is None:
+        return False
+
+    if digit_regex.search(password) is None:
+        return False
+
+    return True
