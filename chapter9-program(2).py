2b048c6ea3bbcccfc1c149490fe6eac9c015356e
Kalel <kalelacez@gmail.com>
2026-07-21 09:17:30 +0100

    chapter 9, project 2 - final answer

--- chapter9-program(2).py
+++ chapter9-program(2).py
@@ -3,11 +3,24 @@ import re
 
 text = "The project started on 3/14/2030. The first review took place on 03-28-2030, followed by another meeting on 2030/4/7. The deadline was moved to 5-12-2030, and the final presentation was scheduled for 2030/06/21. All documents must be submitted by 7/1/2030."
 
-# wrong date formats: 3/14/2030 | 04-14-2030 | 2030/3/14 | 14.03.2030
+# wrong date formats: 3/14/2030 | 2030/4/7 | 7/1/2030 | 2030/06/21 | 5-12-2030 | 03-28-2030
 
-patternObj = re.compile(r'(\d\d?\d?\d?).(\d\d?\d?\d?).(\d\d?\d?\d?)') # or \d{1,4}
+allDates = re.compile(
+    r'(\d{4}).(\d{1,2}).(\d{1,2})'
+    r'|'
+    r'(\d{1,2}).(\d{1,2}).(\d{4})')
 
-formatedDates = patternObj.sub(r'\1/\2/\3', text)
-print(formatedDates)
+
+def correctedDates(match):
+    if match.group(1):
+        year, month, day = match.group(1, 2, 3)
+    else:
+        month, day, year = match.group(4, 5, 6)
+    return f"{int(day):02d}/{int(month):02d}/{int(year):04d}"
+
+allCorrectedDates = allDates.sub(correctedDates, text)
+print(allCorrectedDates)
+
+# HOLYYY JESUSSSSSS I DID ITTT OMG
 
 
