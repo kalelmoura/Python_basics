6ae588ee24a0f4a991d690db6d41bfab9df150a5
Kalel <kalelacez@gmail.com>
2026-08-25 19:41:30 +0100

    fileSize

---
+++ chapter0-11/fileSize.py
@@ -0,0 +1,12 @@
+import os, re, shutil
+from pathlib import Path
+
+files = os.listdir('/Users/gabrielkalel/Downloads/Documentos')
+
+
+for item in files:
+    item = f'/Users/gabrielkalel/Downloads/Documentos/{item}'
+    sizeMB = os.path.getsize(item) / 1000000
+    if sizeMB >= 1:
+        print(item)
+
