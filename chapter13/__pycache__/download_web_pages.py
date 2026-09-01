c40a9c689e3dd1e9dd75f657a8b259e27837509c
Kalel <kalelacez@gmail.com>
2026-09-01 03:56:05 +0100

    chapter13 - download_web_pages through response package

---
+++ chapter13/__pycache__/download_web_pages.py
@@ -0,0 +1,11 @@
+import requests
+
+response = requests.get(('https://automatetheboringstuff.com/files/rj.txt'))
+# Response object
+print(type(response))
+# True if succeeded | False if failed
+print(bool(response.status_code == requests.codes.ok))
+# how many characters the URL has
+print(len(response.text))
+# from character 1 (index 0) to 210 (index 209)
+print(response.text[:210])
